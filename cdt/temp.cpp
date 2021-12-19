d0cd8b0bcf4bc860da6f97b48e4fefc8c06b0a9b
vipul-prog <vipulanand@live.in>
2021-12-19 15:00:39 +0200

    Array Container

--- cdt/temp.cpp
+++ cdt/temp.cpp
@@ -2,11 +2,13 @@
 
 using namespace std;
 
+int foo[] = {1,2,3,4,5,6,78};
+
 
 int main() {
 	int i,j = 0;
 //	++i;
 //	j++;
-	cout << "I " << i++ << "\n";
-	cout << "J " << ++j << "\n";
+	for (int elem : foo) cout << "The value " << elem << "\n";
+
 }
